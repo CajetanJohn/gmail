1f042884bcde44edac604bc891693b1744af0a72
Cajetan <johncajetan1@gmail.com>
2023-12-27 14:35:48 +0000

    today

--- src/Components/Navigation/TopNav/TopNav.js
+++ src/Components/Navigation/TopNav/TopNav.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Style.css';
 import { logo } from '../../../Assets/Images/ExportImages';
-import { searchIcon, filterIcon, SettingsIcon, supportIcon, menuIcon } from '../../../Assets/Icons/ExportIcons';
+import { SearchIcon, FilterIcon, SettingsIcon, SupportIcon, MenuIcon } from '../../../Assets/Icons/ExportIcons';
 
 const TopNav = () => {
   return (
     <div className="top-nav">
         <div className='navigation-logo'>
-          <menuIcon/>
+          <MenuIcon className='icon'/>
           <img src={logo} alt='Gmail logo' className='gmail-logo'/>
-
         </div>
         <div className='navigation-search'>
-            <i class="fa fa-search icon search-icon" aria-hidden="true"></i>
-            <input type='text' placeholder='Search mail'/>
+          <SearchIcon className='icon'/>
+          <input type='text' placeholder='Search mail' className='search-input'/>
+          <FilterIcon className='icon'/>
         </div>
         <div className='navigation-buttons'>
 
